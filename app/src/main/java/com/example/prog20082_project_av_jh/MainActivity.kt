451fdc7bf685f1a4f0ffc242b9090d64d3e2a41e
James Hoang <j.hoang2080@gmail.com>
2020-11-18 11:35:26 -0500

    Init

---
+++ app/src/main/java/com/example/prog20082_project_av_jh/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.prog20082_project_av_jh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
