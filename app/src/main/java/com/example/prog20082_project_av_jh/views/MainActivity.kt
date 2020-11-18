f3375ee24dd997b4baf98f7c66b63d6643bb190e
James Hoang <j.hoang2080@gmail.com>
2020-11-18 13:19:03 -0500

    Error:
    Installation did not succeed.
    The application could not be installed: INSTALL_FAILED_MISSING_SHARED_LIBRARY
    
    List of apks:
    [0] 'E:\Code\Kotlin\PROG20082_Project_AV_JH\app\build\outputs\apk\debug\app-debug.apk'
    Installation failed due to: 'null'
    
    - will continue to code and work around without testing

--- app/src/main/java/com/example/prog20082_project_av_jh/views/MainActivity.kt
+++ app/src/main/java/com/example/prog20082_project_av_jh/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.prog20082_project_av_jh
+package com.example.prog20082_project_av_jh.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.prog20082_project_av_jh.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
