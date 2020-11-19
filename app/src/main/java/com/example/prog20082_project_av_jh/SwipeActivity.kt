6111a07131c415b7b4b399ea1af758158498d049
AngelaVilladiego <villadia@sheridancollege.ca>
2020-11-19 00:05:13 -0500

    Cleaned up application to prepare for merging

--- app/src/main/java/com/example/prog20082_project_av_jh/SwipeActivity.kt
+++ app/src/main/java/com/example/prog20082_project_av_jh/SwipeActivity.kt
@@ -1,10 +1,10 @@
-package com.example.prog20082_project_av_jh.views
+package com.example.prog20082_project_av_jh
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.prog20082_project_av_jh.R
 
-class MainActivity : AppCompatActivity() {
+class SwipeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
