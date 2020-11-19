001e412b65e8dc8a926f0458890d260d7823116a
James Hoang <j.hoang2080@gmail.com>
2020-11-18 20:27:57 -0500

    - Created landing page, and sign up page

--- app/src/main/java/com/example/prog20082_project_av_jh/views/LandingActivity.kt
+++ app/src/main/java/com/example/prog20082_project_av_jh/views/LandingActivity.kt
@@ -4,17 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.prog20082_project_av_jh.R
 
-class SignUpActivity : AppCompatActivity() {
-    val TAG : String = this@SignUpActivity.toString()
-
+class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-
-//        this.initialSetup()
+        setContentView(R.layout.activity_landing)
     }
-
-//    fun initialSetup(){
-//
-//    }
 }
