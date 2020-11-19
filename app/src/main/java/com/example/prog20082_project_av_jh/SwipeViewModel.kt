fa0ead814d60ba9829ae43cdde70d4719ade0687
AngelaVilladiego <villadia@sheridancollege.ca>
2020-11-18 23:42:06 -0500

    Notifications view, both swipe fragments, views created

---
+++ app/src/main/java/com/example/prog20082_project_av_jh/SwipeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.prog20082_project_av_jh
+
+import androidx.lifecycle.ViewModel
+
+class SwipeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/prog20082_project_av_jh/views/SwipeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prog20082_project_av_jh.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.prog20082_project_av_jh.R
+
+class SwipeActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
