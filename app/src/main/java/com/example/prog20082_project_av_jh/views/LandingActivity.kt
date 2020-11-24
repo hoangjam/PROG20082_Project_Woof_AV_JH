e619ae5c1dc19f5d9d9c60357e7fd0291221ed9b
James Hoang <j.hoang2080@gmail.com>
2020-11-24 02:42:41 -0500

    - Added InfoFragment - Progress on LandingActivity, need to match layout button names and coded button names - OnClick needs to go to the first "match" card

--- app/src/main/java/com/example/prog20082_project_av_jh/views/LandingActivity.kt
+++ app/src/main/java/com/example/prog20082_project_av_jh/views/LandingActivity.kt
@@ -1,12 +1,42 @@
 package com.example.prog20082_project_av_jh.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+//import androidx.lifecycle.ViewModel
 import com.example.prog20082_project_av_jh.R
 
-class LandingActivity : AppCompatActivity() {
+class LandingActivity : AppCompatActivity(), View.OnClickListener {
+
+    val TAG: String = this@LandingActivity.toString()
+
+    lateinit var  btnSignUp: Button
+//    lateinit var userViewModel: ViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+        btnSignUp = findViewById(R.id.btnSignUp)
+        btnSignUp.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null){
+            if(v.id == btnSignUp.id){
+                this.goToSignUp()
+            }
+//            else if(v.id == btnSignIn.id){
+//                this.goToSignIn()
+//            }
+        }
+    }
+
+    private fun goToSignUp(){
+        val signUpIntent = Intent(this, SignUpActivity::class.java)
+        startActivity(signUpIntent)
     }
 }
