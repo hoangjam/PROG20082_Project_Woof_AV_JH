664e38501a557c04e68d995326a7a92a391b8c74
AngelaVilladiego <villadia@sheridancollege.ca>
2020-11-25 19:06:45 -0500

    Merged from Master

--- app/src/main/java/com/example/prog20082_project_av_jh/SwipeFragment.kt
+++ app/src/main/java/com/example/prog20082_project_av_jh/SwipeFragment.kt
@@ -19,7 +19,7 @@ class SwipeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.swipe_full_fragment, container, false)
+        return inflater.inflate(R.layout.swipe_half_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
