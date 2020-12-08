bffcccaf24ae150608f8d0a80d07ab89043ad2fa
AngelaVilladiego <villadia@sheridancollege.ca>
2020-12-08 16:35:06 -0500

    Completed navigation from matched profile fragment to location map fragment

--- app/src/main/java/com/example/prog20082_project_av_jh/ui/MatchedProfileFragment.kt
+++ app/src/main/java/com/example/prog20082_project_av_jh/ui/MatchedProfileFragment.kt
@@ -5,35 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.prog20082_project_av_jh.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_matched_profile.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
  * Use the [MatchedProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MatchedProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class MatchedProfileFragment : Fragment(), View.OnClickListener {
+
+    lateinit var fabToMap : FloatingActionButton
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_matched_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_matched_profile, container, false)
+
+        fabToMap = view.fabLocation
+        fabToMap.setOnClickListener(this)
+
+        return view
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null){
+            when (v.id) {
+                fabToMap.id -> {
+                    goToLocation()
+                }
+            }
+        }
+    }
+
+    private fun goToLocation() {
+        Navigation.findNavController(requireView()).navigate(R.id.action_nav_matched_profile_to_nav_maps)
     }
 
     companion object {
@@ -41,17 +57,15 @@ class MatchedProfileFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
+         *
          * @return A new instance of fragment MatchedProfileFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
                 MatchedProfileFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+
                     }
                 }
     }
