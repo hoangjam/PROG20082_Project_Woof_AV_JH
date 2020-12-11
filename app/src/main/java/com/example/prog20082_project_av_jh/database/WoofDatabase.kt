af53d78ba45c66fca48b159cc77f8a136c9dd0ad
James Hoang <j.hoang2080@gmail.com>
2020-12-11 16:44:55 -0500

    created SharedPreferencesManager.kt edited ProfileFragment.kt, enabled editing/retrieving/saving <- does not populate

--- app/src/main/java/com/example/prog20082_project_av_jh/database/WoofDatabase.kt
+++ app/src/main/java/com/example/prog20082_project_av_jh/database/WoofDatabase.kt
@@ -13,7 +13,7 @@ Name: Jameson Hoang
 Student ID: 991548515
 Date: 2020-11-25
 */
-@Database(entities = arrayOf(User::class), version = 3)
+@Database(entities = arrayOf(User::class), version = 4)
 @TypeConverters(Converters::class)
 abstract class WoofDatabase : RoomDatabase() {
 
